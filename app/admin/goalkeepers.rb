ff9d557c3b4b38d8140268db45876c4fb8fc0797
Raghu Betina <raghu@firstdraft.com>
2019-03-21 08:32:08 +0000

    Whitelist columns in Goalkeeper for ActiveAdmin

--- app/admin/goalkeepers.rb
+++ app/admin/goalkeepers.rb
@@ -1,4 +1,6 @@
 ActiveAdmin.register Goalkeeper do
+
+ permit_params :position_id
 # See permitted parameters documentation:
 # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
 #
